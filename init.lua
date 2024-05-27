0aa1ba6da2d73846d7151714350851f7b80cf013
liuk6 <liuk6@xiaopeng.com>
2024-05-27 16:48:41 +0800

    Simplify cmp config

--- init.lua
+++ init.lua
@@ -544,11 +544,6 @@ require('lazy').setup({
     config = function()
       require('luasnip.loaders.from_vscode').load()
 
-      local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-      end
-
       local kind_icons = {
         Text = '',
         Method = '󰆧',
@@ -607,7 +602,6 @@ require('lazy').setup({
       }
 
       local cmp = require('cmp')
-      local luasnip = require('luasnip')
       cmp.setup({
         preselect = cmp.PreselectMode.None,
         snippet = {
@@ -623,29 +617,15 @@ require('lazy').setup({
           { name = 'path' },
         },
         mapping = {
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-
-          ['<CR>'] = cmp.mapping.confirm({ select = false }),
+          ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ['<CR>'] = cmp.mapping(
+            cmp.mapping.confirm({
+              behavior = cmp.ConfirmBehavior.Insert,
+              select = true,
+            }),
+            { 'i', 'c' }
+          ),
         },
         experimental = {
           ghost_text = true,

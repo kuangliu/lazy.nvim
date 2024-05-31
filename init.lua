6af13f2c94db56648249d7960a7b267fdb940a50
liuk6 <liuk6@xiaopeng.com>
2024-05-31 16:25:00 +0800

    Config simplify

--- init.lua
+++ init.lua
@@ -385,17 +385,8 @@ require('lazy').setup({
   {
     'Shatur/neovim-session-manager',
     config = function()
-      local Path = require('plenary.path')
       require('session_manager').setup({
-        sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'),
-        path_replacer = '__',
-        colon_replacer = '++',
         autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
-        autosave_last_session = true,
-        autosave_ignore_not_normal = true,
-        autosave_ignore_filetypes = { 'gitcommit' },
-        autosave_only_in_session = false,
-        max_path_length = 80,
       })
 
       local config_group = vim.api.nvim_create_augroup('MyConfigGroup', {})

f7fb1793bfdf9a48c9b80c0c5fbabcf693ffed7f
liuk6 <liuk6@xiaopeng.com>
2024-06-27 15:51:48 +0800

    Update cmp

--- init.lua
+++ init.lua
@@ -540,6 +540,7 @@ require('lazy').setup({
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
       'kuangliu/friendly-snippets',
@@ -657,6 +658,22 @@ require('lazy').setup({
             scrollbar = '║',
           },
         },
+      })
+
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' },
+        },
+      })
+
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' },
+          { name = 'cmdline' },
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false },
       })
     end,
   },
